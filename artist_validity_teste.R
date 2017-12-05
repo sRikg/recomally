2babcd53a1233d7458fd1aa565df5781da6517cf
Srik G <sskanthgorthy@gmail.com>
2017-12-05 16:33:42 +0530

    Update artist_validity_teste.R

--- artist_validity_teste.R
+++ artist_validity_teste.R
@@ -1,2 +1,9 @@
 artist_validity_tester <- function(data_frame, artist){
+  temp <- data_frame[data_frame$Artist.Name == artist,]
+  temp_num <- sum(temp$cleaned.likes %in% unique(temp$Artist.Id))
+  if(temp_num == length(unique(temp$cleaned.User.Id))) {
+    print("valid") 
+  } else {
+    print("invalid")
+  }
 }
