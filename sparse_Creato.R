1caec65cf35276005eb2affe16516dd66aa6e841
Srik G <sskanthgorthy@gmail.com>
2017-12-05 17:25:28 +0530

    Update sparse_Creato.R

--- sparse_Creato.R
+++ sparse_Creato.R
@@ -1,2 +1,27 @@
 sparse_Creator <- function(data_frame, col_num_user, col_num_likes){
+  data <- data_frame[,c(col_num_user, col_num_likes)]
+  
+  users <- unique(data[,1])
+  likes <- unique(data[,2])
+  
+  dt <- as.data.frame(
+    matrix(
+      ncol = length(likes), 
+      nrow = length(users), 
+      0
+    )
+  )
+  
+  temp <- lapply(
+    1:length(users), 
+    function(i) (likes %in% data[data[,1] == users[i], 2])*1
+    )
+  print("Sparse Values generated")
+  
+  for(i in 1:length(users)){
+    dt[i,] <- temp[[i]]
+  print(".", i)
+  }
+  
+  return(dt)
 }
