09ebcf9196bc3f863eae9b2db0430dc1cb543961
Srik G <sskanthgorthy@gmail.com>
2017-12-05 15:29:54 +0530

    Update Pulle.R

--- Pulle.R
+++ Pulle.R
@@ -1,4 +1,25 @@
 # depending on what is given choose the option
-puller <- fucntion(read_file_type, readable, ...){
-
+# 1 - CSV, 2 - XLSX, 3 - Google Sheets
+puller <- function(read_file_type, readable, sheetIndex = NA, ...){
+  if(read_file_type == 1){
+    worthy <- read.csv(readable)
+  } else if(read_file_type == 2){
+    require(xlsx)
+    if(sum(is.na(sheetIndex)) == 1) {
+      print("Error, please provide Sheet Index")
+    } else {
+      worthy <- read.xlsx(readable, sheetIndex = sheetIndex)
+    }
+  } else if(read_file_type == 3) {
+    require(googlesheets)
+    temp <- gs_title(readable)
+    if(sum(is.na(sheetIndex)) == 1) {
+      print("Error, please provide Sheet Index")
+    } else {
+      worthy <- gs_read(temp, sheetIndex)
+    }
+    worthy <- as.data.frame(worthy)
+  } else {
+    print("Provide a valid read_file_type")
+  }
 }
