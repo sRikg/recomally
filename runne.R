641c58b890714494e067443bbb6abdf6d629568d
Srik G <sskanthgorthy@gmail.com>
2017-12-05 16:37:54 +0530

    Update runne.R

--- runne.R
+++ runne.R
@@ -8,3 +8,22 @@ rm(df2)
 rm(df3)
 
 df <- df[,-1]
+df <- unique(df)
+df[,6] <- paste0("https://", df[,6])
+
+df[,2] <- html_correcter(df[,2])
+df[,5] <- html_correcter(df[,5])
+df[,6] <- html_correcter(df[,6])
+
+valids <- sapply(
+  1:length(unique(df[,2])), 
+  function(i) artist_validity_tester(
+    df, 
+    unique(df[,2])[i])
+  )
+
+validation <- sum(sum(valids == "valid") == length(unique(df[,1])))
+rm(valids)
+# if(validation) == 1 {proceed} else {recheck data}
+rm(valdiation)
+
