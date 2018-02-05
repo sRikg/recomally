07df52cb0de44d03eea530f9ad993177f84e56b9
Srik G <sskanthgorthy@gmail.com>
2018-02-05 17:29:07 +0530

    Update artist_Validity_Teste.R

--- artist_Validity_Teste.R
+++ artist_Validity_Teste.R
@@ -1,6 +1,6 @@
 artist_validity_tester <- function(data_frame, artist){
-  temp <- data_frame[data_frame$Artist.Name == artist,]
-  temp_num <- sum(temp$cleaned.likes %in% unique(temp$Artist.Id))
+  temp <- data_frame[data_frame$`Artist Name` == artist,]
+  temp_num <- sum(temp$page_Link %in% unique(temp$new_Artist_Id))
   if(temp_num == length(unique(temp$cleaned.User.Id))) {
     print("valid") 
   } else {
