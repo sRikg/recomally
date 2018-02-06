efa53c1f4d72161a73bd7f8143ea0b65220a8712
Srik G <sskanthgorthy@gmail.com>
2018-02-06 10:49:09 +0530

    Update artist_Validity_Teste.R

--- artist_Validity_Teste.R
+++ artist_Validity_Teste.R
@@ -8,8 +8,24 @@ artist_validity_tester <- function(data_frame, artist){
   }
 }
 
+valids <- as.data.frame(
+  cbind(
+    unique(collated_merged[,1]),
+    valids
+  )
+)
+names(valids) <- c("Artist Name", "valids")
+
 artist_valid_num <- function(data_frame, artist){
   temp <- data_frame[data_frame$`Artist Name` == artist,]
   temp_num <- sum(temp$page_Link %in% unique(temp$new_Artist_Id))
   return(c(temp_num, length(unique(temp$cleaned.User.Id))))
 }
+
+valids[,3:4] <- t(
+  sapply(
+    1:length(valids[,1]),
+    function(i) artist_valid_num(collated_merged, valids[i,1])
+    )
+)
+
