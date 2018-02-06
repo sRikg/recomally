f1ed39a69e6c8164c75a02a1784412a66094afb2
Srik G <sskanthgorthy@gmail.com>
2018-02-06 10:45:31 +0530

    Update artist_Validity_Teste.R

--- artist_Validity_Teste.R
+++ artist_Validity_Teste.R
@@ -1,4 +1,3 @@
-
 artist_validity_tester <- function(data_frame, artist){
   temp <- data_frame[data_frame$`Artist Name` == artist,]
   temp_num <- sum(temp$page_Link %in% unique(temp$new_Artist_Id))
@@ -7,4 +6,10 @@ artist_validity_tester <- function(data_frame, artist){
   } else {
     print("invalid")
   }
+}
+
+artist_valid_num <- function(data_frame, artist){
+  temp <- data_frame[data_frame$`Artist Name` == artist,]
+  temp_num <- sum(temp$page_Link %in% unique(temp$new_Artist_Id))
+  return(c(temp_num, length(unique(temp$cleaned.User.Id))))
 }
